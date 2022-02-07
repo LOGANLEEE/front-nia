fae290175ca873b5103116459786fcb316304682
LOGANLEEE <logan_lee@kakao.com>
2022-02-08 00:53:34 +0900

    init template

---
+++ src/components/Template/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Wrapper } from './Wrapper';
+
+interface Props {
+	className?: string;
+}
+
+export const Template = ({ className }: Props) => {
+	return <Wrapper className={className}>Template</Wrapper>;
+};
