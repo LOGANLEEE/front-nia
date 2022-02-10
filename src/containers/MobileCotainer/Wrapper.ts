86389ccf315588d7ef475d074e0e8ce1ba58843c
LOGANLEEE <logan_lee@kakao.com>
2022-02-11 00:06:08 +0900

    receive theme props

--- src/containers/MobileCotainer/Wrapper.ts
+++ src/containers/MobileCotainer/Wrapper.ts
@@ -3,4 +3,15 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
 	width: 100vw;
 	height: 100vh;
+
+	background-color: ${({
+		theme: {
+			dark: { bg },
+		},
+	}) => bg};
+	color: ${({
+		theme: {
+			dark: { fg },
+		},
+	}) => fg};
 `;
