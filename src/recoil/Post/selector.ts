1818e46b7095a1acc553bd918e0079cbc020fd9b
LOGANLEEE <logan_lee@kakao.com>
2022-03-09 16:43:12 -0500

    imprv selector

--- src/recoil/Post/selector.ts
+++ src/recoil/Post/selector.ts
@@ -1,17 +1,13 @@
 import { selector } from 'recoil';
-import { postAtom, PostType } from 'recoil/Post/atom';
 import { fetch } from 'utils/axiosClient';
 
-export const postSelector = {
-	get_posts: selector({
-		key: 'post/postSelector/get_posts',
-		get: () => fetch.get('post').then(({ data }) => data),
-		set: ({ get, set }, val) => {
-			val as PostType[];
-			const { post } = get(postAtom);
-			if (!val.every((value: PostType, idx: number) => value === post[idx])) {
-				set(postAtom, { post: val });
-			}
-		},
-	}),
-};
+export const getPosts = selector({
+	key: 'selector/post/getPosts',
+	get: async () => {
+		const { data, status } = await fetch.get('post').catch(() => ({ data: [], status: -1 }));
+		if (status === 200) {
+			return { data, isError: false };
+		}
+		return { data: [], isError: true };
+	},
+});
