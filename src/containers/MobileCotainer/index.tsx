97940a9e6fe770deae9e2bf9797e598849d63387
LOGANLEEE <logan_lee@kakao.com>
2022-03-09 16:42:13 -0500

    remove comment

--- src/containers/MobileCotainer/index.tsx
+++ src/containers/MobileCotainer/index.tsx
@@ -1,7 +1,5 @@
-import { ErrorFallBack } from 'components/ErrorFallBack';
 import { PostView } from 'components/PostView';
 import React from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
 
 import { Wrapper } from './Wrapper';
 
@@ -12,13 +10,7 @@ interface Props {
 export const MobileCotainer = ({ className }: Props) => {
 	return (
 		<Wrapper className={className}>
-			{/* <ErrorBoundary
-				FallbackComponent={ErrorFallBack}
-				onReset={() => {
-					// reset the state of your app so the error doesn't happen again
-				}}> */}
 			<PostView />
-			{/* </ErrorBoundary> */}
 		</Wrapper>
 	);
 };
