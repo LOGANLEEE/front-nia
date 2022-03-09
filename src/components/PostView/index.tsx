bf35490d20361dfef1a823d94065b05c649cda87
LOGANLEEE <logan_lee@kakao.com>
2022-03-09 16:41:41 -0500

    imprv fetching logic

--- src/components/PostView/index.tsx
+++ src/components/PostView/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
-import { useRecoilStateLoadable } from 'recoil';
-import { PostType } from 'recoil/Post/atom';
-import { postSelector } from 'recoil/Post/selector';
+import { format } from 'date-fns';
+import React, { useCallback, useEffect, useMemo } from 'react';
+import { useRecoilState, useRecoilStateLoadable, useRecoilValueLoadable } from 'recoil';
+import { postAtom, PostType } from 'recoil/Post/atom';
+import { getPosts } from 'recoil/Post/selector';
 
 import { Wrapper } from './Wrapper';
 
@@ -10,31 +11,42 @@ interface Props {
 }
 
 export const PostView = ({ className }: Props) => {
-	const [post, setPost] = useRecoilStateLoadable(postSelector.get_posts);
+	const [{ post }, setPost] = useRecoilState(postAtom);
+	const {
+		state,
+		contents: { data, isError },
+	} = useRecoilValueLoadable(getPosts);
 
 	useEffect(() => {
-		post.state === 'hasValue' && setPost(post.contents);
-	}, [post.contents, post.state, setPost]);
+		if (state === 'hasValue' && !isError) {
+			setPost((prev) => ({ ...prev, post: data }));
+		}
+	}, [state, isError, data, setPost]);
+
+	const numberFormatter = useCallback((hit: number) => new Intl.NumberFormat('en-US').format(hit), []);
+
+	const PostRender = useCallback(({ author, from, hit, link, title, content, id, isnew, upload_date }: PostType) => {
+		return (
+			<li className='list' key={id} onClick={() => window.open(link, title)}>
+				<div className='from'>{from}</div>
+				<div className='info'>
+					<div className='title'>{title}</div>
+					<br />
+					<div className='bottom-detail'>
+						<div className='date'>{upload_date && format(new Date(upload_date), 'MM/dd yy:mm')}</div>
+						<div className='author'>{author}</div>
+						<div className='hit'>{`🔥${numberFormatter(hit)}`}</div>
+					</div>
+				</div>
+			</li>
+		);
+	}, []);
 
 	return (
 		<Wrapper className={className}>
-			{post.state === 'loading' && <div>loading...</div>}
-			{post.state === 'hasError' && <div>Error...</div>}
-			{post.state === 'hasValue' && (
-				<ul>
-					{post.contents.map(({ author, from, hit, link, title, content, id, isnew, upload_date }: PostType) => (
-						<li className='list' key={id} onClick={() => window.open(link, title)}>
-							{author}
-							{from}
-							{content}
-							{isnew}
-							{upload_date}
-							{title}
-							{hit}
-						</li>
-					))}
-				</ul>
-			)}
+			{post?.map((e) => (
+				<PostRender key={e.id} {...e} />
+			))}
 		</Wrapper>
 	);
 };
