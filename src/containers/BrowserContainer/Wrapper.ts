771f5dd6e05dc3f043ebb22aca14c1629ac5b046
LOGANLEEE <logan_lee@kakao.com>
2022-03-09 16:42:06 -0500

    apply theme

--- src/containers/BrowserContainer/Wrapper.ts
+++ src/containers/BrowserContainer/Wrapper.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+	width: 100vw;
+	background-color: ${({
+		theme: {
+			dark: { bg },
+		},
+	}) => bg};
+	color: ${({
+		theme: {
+			dark: { fg },
+		},
+	}) => fg};
+`;
