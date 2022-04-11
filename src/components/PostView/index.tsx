5c0c20e6dda31488dc9e2d177eab02419c92caee
LOGANLEEE <logan_lee@kakao.com>
2022-04-11 16:42:29 -0400

    fix type problem

--- src/components/PostView/index.tsx
+++ src/components/PostView/index.tsx
@@ -1,6 +1,7 @@
 import { format } from 'date-fns';
-import React, { useCallback, useEffect, useMemo } from 'react';
-import { useRecoilState, useRecoilStateLoadable, useRecoilValueLoadable } from 'recoil';
+import React, { useCallback, useEffect } from 'react';
+import { Bars } from 'react-loader-spinner';
+import { useRecoilState, useRecoilValueLoadable } from 'recoil';
 import { postAtom, PostType } from 'recoil/Post/atom';
 import { getPosts } from 'recoil/Post/selector';
 
@@ -18,35 +19,39 @@ export const PostView = ({ className }: Props) => {
 	} = useRecoilValueLoadable(getPosts);
 
 	useEffect(() => {
-		if (state === 'hasValue' && !isError) {
-			setPost((prev) => ({ ...prev, post: data }));
-		}
+		if (state === 'hasValue' && !isError) setPost((prev) => ({ ...prev, post: data }));
 	}, [state, isError, data, setPost]);
 
 	const numberFormatter = useCallback((hit: number) => new Intl.NumberFormat('en-US').format(hit), []);
 
-	const PostRender = useCallback(({ author, from, hit, link, title, content, id, isnew, upload_date }: PostType) => {
-		return (
-			<li className='list' key={id} onClick={() => window.open(link, title)}>
-				<div className='from'>{from}</div>
-				<div className='info'>
-					<div className='title'>{title}</div>
-					<br />
-					<div className='bottom-detail'>
-						<div className='date'>{upload_date && format(new Date(upload_date), 'MM/dd yy:mm')}</div>
-						<div className='author'>{author}</div>
-						<div className='hit'>{`🔥${numberFormatter(hit)}`}</div>
-					</div>
-				</div>
-			</li>
-		);
-	}, []);
+	// do virtualized list
+	const PostRender = useCallback(
+		({ post }: { post: PostType[] }): JSX.Element => (
+			<ul>
+				{post.map(({ author, from, hit, link, title, content, id, isnew, upload_date }: PostType) => (
+					<li className='list' key={id} onClick={() => window.open(link, title)}>
+						<div className='from'>{from}</div>
+						<div className='info'>
+							<div className='title'>{title}</div>
+							<br />
+							<div className='bottom-detail'>
+								<div className='date'>{upload_date && format(new Date(upload_date), 'MM/dd yy:mm')}</div>
+								<div className='author'>{author}</div>
+								<div className='hit'>{`🔥${numberFormatter(hit)}`}</div>
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
+		),
+
+		[],
+	);
 
 	return (
 		<Wrapper className={className}>
-			{post?.map((e) => (
-				<PostRender key={e.id} {...e} />
-			))}
+			{state === 'loading' && <Bars height='100' width='100' color='grey' ariaLabel='loading-indicator' />}
+			{state === 'hasValue' && <PostRender post={post} />}
 		</Wrapper>
 	);
 };
